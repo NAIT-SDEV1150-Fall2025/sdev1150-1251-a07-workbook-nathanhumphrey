ec91b5fc76c20192a69c61adbe88abeadb705c9a
Nathan Humphrey <nathpaulhump@gmail.com>
2025-09-29 11:18:57 -0600

    Lesson 08 toggle click

---
+++ lesson-08/src/main.js
@@ -0,0 +1,34 @@
+console.log('Lesson 08 starter loaded');
+
+// 1. load event (document ready) - NOTE this is unnecessary if using `defer` in the script tag or using module type
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('DOM is ready');
+  // 2. Selecting elements
+  const btnToggle = document.querySelector('#btn-toggle');
+  const btnMessage = document.querySelector('#btn-message');
+  const message = document.querySelector('#message');
+  const hoverCard = document.querySelector('#hover-card');
+  const hoverStatus = document.querySelector('#hover-status');
+  const keyOutput = document.querySelector('#key-output');
+  const list = document.querySelector('#list');
+  const selection = document.querySelector('#selection');
+
+  // 3. click: toggle a highlight class on the body
+  btnToggle.addEventListener('click', function () {
+    document.body.classList.toggle('highlight');
+    const toggleOn = document.body.classList.contains('highlight');
+
+    if (toggleOn === true) {
+      btnToggle.textContent = 'Highlight is ON';
+    } else {
+      btnToggle.textContent = 'Highlight is OFF';
+    }
+  });
+  // 4. click: change message textContent (no HTML parsing)
+
+  // 5. mouseover / mouseout: display hover status on the card
+
+  // 6. keydown: show last key pressed (global listener)
+
+  // 7. Event delegation: one listener on the <ul> for all <li> elements
+});
