a1de5003505e35eb6460a732d75f0ca2527ebb90
Nathan Humphrey <nathpaulhump@gmail.com>
2025-10-06 11:16:23 -0600

    Update lesson 10 - capture basic values

--- lesson-10/src/main.js
+++ lesson-10/src/main.js
@@ -2,8 +2,19 @@ console.log('Lesson 10 starter loaded');
 
 // 1. Select required elements
 const form = document.querySelector('#contact-form');
+const result = document.querySelector('#result');
 
 // 2. Function to gather and structure form data
+function serializeForm(formEl) {
+  // get the name, email, and bio
+  const fullNameValue = formEl.elements.fullName.value;
+  // TODO: get the email and bio
+
+  // Add the remaining fields here
+  return {
+    fullName: fullNameValue,
+  };
+}
 
 // Access values using both form.elements and query selectors
 
@@ -12,6 +23,14 @@ const form = document.querySelector('#contact-form');
 // Prevent default behavior (navigation/reload) using event.preventDefault()
 form.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  const data = serializeForm(form);
+
+  // TODO: display the remaining values
+  result.textContent = `
+    Submission received:
+    - Name: ${data.fullName}
+  `;
 });
 
 // 4. Handle form reset - reset the result area text when the form is reset
