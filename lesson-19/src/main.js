c2657a422feafeb5252b1c3dd2ea78cf0f72dd63
Nathan Humphrey <NathanH@nait.ca>
2025-11-12 11:30:27 -0700

    Complete Lesson 19 Button

--- lesson-19/src/main.js
+++ lesson-19/src/main.js
@@ -1,18 +1,6 @@
+import './primary-button.js';
 // Import the user-card component to register the custom element
 
 // Create an additional user card using HTML and append it to the main element
 
 // Create another user card using JavaScript DOM API only and append it to the main element
-
-const template = document.querySelector('#primary-button-template');
-
-// PrimaryButton Demo
-class PrimaryButton extends HTMLElement {
-  constructor() {
-    super();
-    const content = template.content.cloneNode(true);
-    const shadow = this.attachShadow({ mode: 'open' });
-    shadow.appendChild(content);
-  }
-}
-customElements.define('primary-button', PrimaryButton);
---
+++ lesson-19/src/primary-button.js
@@ -0,0 +1,27 @@
+const template = document.createElement('template');
+template.innerHTML = `
+<style>
+  button {
+    border-radius: 0.25rem;
+    border-color: #06295c;
+    background-color: #1875ff;
+    color: #ffffff;
+    padding:  0.5rem;
+  }
+</style>
+<button>
+  <slot name="text">Button Text</slot>
+</button>`;
+
+// PrimaryButton Demo
+class PrimaryButton extends HTMLElement {
+  constructor() {
+    super();
+    const content = template.content.cloneNode(true);
+    const shadow = this.attachShadow({ mode: 'open' });
+    shadow.appendChild(content);
+  }
+}
+customElements.define('primary-button', PrimaryButton);
+
+export default PrimaryButton;
